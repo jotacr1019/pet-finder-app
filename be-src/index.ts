d1e815312515bfe2140dc6c39f5750d7bcb19989
Jose Fernandez <jotaj1019@gmail.com>
2023-07-24 15:24:26 -0600

    25th commit, changes in be-src/index.ts

--- be-src/index.ts
+++ be-src/index.ts
@@ -343,20 +343,20 @@ async function sendMessageToUser(messageData, reporter_email) {
 }
 myApp.use(express.static("dist"));
 
-myApp.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
+// myApp.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../dist/index.html"));
+// });
 
 // myApp.get("*", function (req, res) {
 //     const route = path.resolve(__dirname, "../dist/index.html"); // only dist?
 //     res.sendFile(route);
 // });
 
-// const staticDirPath = path.resolve(__dirname, "../dist"); //dist!!
-// myApp.use(express.static(staticDirPath)); // para usar sin parcel
-// myApp.get("*", function (req, res) {
-//     res.sendFile(staticDirPath + "/index.html");
-// });
+const staticDirPath = path.resolve(__dirname, "../dist"); //dist!!
+myApp.use(express.static(staticDirPath)); // para usar sin parcel
+myApp.get("*", function (req, res) {
+    res.sendFile(staticDirPath + "/index.html");
+});
 
 myApp.listen(port);
 console.log("API escuchando en el puerto " + port);
