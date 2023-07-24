a89c99eeffcb3d530363cff6848514142bdc784d
Jose Fernandez <jotaj1019@gmail.com>
2023-07-24 16:12:48 -0600

    29th commit, changes in be-src/index.ts

--- be-src/index.ts
+++ be-src/index.ts
@@ -341,16 +341,16 @@ async function sendMessageToUser(messageData, reporter_email) {
         console.error(error);
     }
 }
-myApp.use(express.static(__dirname, "../dist"));
+// myApp.use(express.static(__dirname, "../dist"));
 
-myApp.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
-
-// myApp.get("*", function (req, res) {
-//     const route = path.resolve(__dirname, "../dist/index.html"); // only dist?
-//     res.sendFile(route);
+// myApp.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../dist/index.html"));
 // });
+
+myApp.get("*", function (req, res) {
+    const route = path.resolve(__dirname, "../dist/index.html"); // only dist?
+    res.sendFile(route);
+});
 
 // const staticDirPath = path.resolve(__dirname, "../dist"); //dist!!
 // myApp.use(express.static(staticDirPath)); // para usar sin parcel
